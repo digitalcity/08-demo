e27664f406b1e71e68b51427ba2b21f589773fc1
newming <a977527479@sina.com>
2016-11-30 15:38:39 +0800

    home

--- src/About/index.js
+++ src/About/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
-import CircularProgress from 'material-ui/CircularProgress';
 
 class About extends React.Component{
+  handleSubmit(e){
+    e.preventDefault();
+    console.log(this.refs.gitname.getValue());
+  }
   render(){
     return(
-      <div>
-        <CircularProgress/>
-        <RaisedButton label='aaa'/>
+      <div className='about-wrap'>
+        <div className='git-card'>
+          <h2>Search Git Info</h2>
+          <form className='about-form' onSubmit={this.handleSubmit.bind(this)}>
+            <TextField ref='gitname' hintText="github username" underlineFocusStyle={{borderColor:'#bada55'}} />
+            <RaisedButton label="search" secondary={true} style={{marginLeft:'10px'}}/>
+          </form>
+        </div>
       </div>
     )
   }
