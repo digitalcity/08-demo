692775fc4d0e5826f5e5e864bb039c050ad3263d
newming <a977527479@sina.com>
2016-12-01 15:05:23 +0800

    work

--- src/Work/index.js
+++ src/Work/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import echarts from 'echarts';
 
 class Work extends React.Component{
+  componentDidMount(){
+    var myChart = echarts.init(document.getElementById('main'));
+    myChart.setOption({
+      title: {
+        text: '我的技能展示',
+        textStyle:{
+          color: 'teal'
+        },
+        left:'center'
+      },
+      tooltip: {},
+      xAxis: {
+          data: ['html','css','js','react','node','mongodb']
+      },
+      yAxis: {},
+      series: [{
+          name: '熟练度',
+          type: 'bar',
+          data: [90, 95, 85, 80, 70, 60]
+      }]
+    });
+  }
   render(){
     return(
-      <div>
-        Work
-      </div>
+      <div id='main' style={{width:'100%',height:'auto',maxWidth:'600px',margin:'0 auto'}}></div>
     )
   }
 }
